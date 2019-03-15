e24507b4b036c28bd6ef693d72251da72c4fbefd
YohanAvishke <yohan.2017213@iit.ac.lk>
2019-03-15 10:43:10 +0530

    Solved Divisible sum pairs

--- src/Algorithms/Implementation/Divisible_Sum_Pairs.java
+++ src/Algorithms/Implementation/Divisible_Sum_Pairs.java
@@ -1,4 +1,21 @@
 package Algorithms.Implementation;
 
 public class Divisible_Sum_Pairs {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int count = 0;
+        for (int i = 0; i < ar.length - 1; i++) {
+            for (int j = 0; j < ar.length; j++) {
+                if (i < j) {
+                    int sum = ar[i] + ar[j];
+                    boolean condi = sum % k == 0;
+                    if (condi) count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    /*public static void main(String[] args) {
+    }*/
 }
