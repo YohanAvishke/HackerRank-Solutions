89dc4ab0eb309c2ea5601f6d85a5e03caffc5756
YohanAvishke <yohan.2017213@iit.ac.lk>
2019-03-19 08:48:12 +0530

    Solved Array Manipulation

---
+++ src/Algorithms/Implementation/Array_Manipulation.java
@@ -0,0 +1,34 @@
+package Algorithms.Implementation;
+
+import java.io.*;
+import java.util.*;
+
+public class Array_Manipulation {
+
+    public static void main(String[] args) throws IOException {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        long[] arr = new long[n];
+        int lower, upper, sum;
+        long max = 0, temp = 0;
+
+        //fill arr with zeros
+        for (int i = 0; i < n; i++) arr[i] = 0;
+
+        for (int i = 0; i < m; i++) {
+            lower = scan.nextInt();
+            upper = scan.nextInt();
+            sum = scan.nextInt();
+            arr[lower - 1] += sum;
+            if (upper < n) arr[upper] -= sum;
+        }
+
+        for (int i = 0; i < n; i++) {
+            temp += arr[i];
+            if (temp > max) max = temp;
+        }
+
+        System.out.println(max);
+    }
+}
