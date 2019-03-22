a7a81a96fd238546981c9136eda2c90feca5533b
YohanAvishke <yohan.2017213@iit.ac.lk>
2019-03-22 11:27:29 +0530

    Solved Java Loops 1

---
+++ src/Java/Introduction/Java_Loops_I.java
@@ -0,0 +1,18 @@
+package Java.Introduction;
+
+import java.util.*;
+
+public class Java_Loops_I {
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(N + " x " + i + " = " + (N * i));
+        }
+
+        scanner.close();
+    }
+}l
