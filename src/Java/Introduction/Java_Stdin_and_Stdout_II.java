e07df111ea36b6e3ef3bac8ce5c1c46e4488c9dd
YohanAvishke <yohan.2017213@iit.ac.lk>
2019-03-22 10:50:35 +0530

    Solved Java Stdin and Stdout 2

---
+++ src/Java/Introduction/Java_Stdin_and_Stdout_II.java
@@ -0,0 +1,17 @@
+package Java.Introduction;
+
+import java.util.*;
+
+public class Java_Stdin_and_Stdout_II {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int i = scan.nextInt();
+        double d = scan.nextDouble();
+        scan.nextLine(); //to clear up the buffer
+        String s = scan.nextLine();
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
