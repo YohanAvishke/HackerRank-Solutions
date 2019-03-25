59840fbb4b58a2ee342a3ac89841492940d84a38
YohanAvishke <yohan.2017213@iit.ac.lk>
2019-03-25 12:44:46 +0530

    fixed error

--- src/Java/Introduction/Welcome_to_Java.java
+++ src/Java/Introduction/Welcome_to_Java.java
@@ -2,6 +2,6 @@ package Java.Introduction;
 
 public class Welcome_to_Java {
     public static void main(String[] args) {
-        System.out.printf("%s%n%s","Hello, World","Hello, Java");
+        System.out.printf("%s%n%s","Hello, World.","Hello, Java.");
     }
 }
