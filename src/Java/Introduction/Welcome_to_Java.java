b5051f24058956071fbe1dfc34bc235d08f3bb5f
YohanAvishke <yohan.2017213@iit.ac.lk>
2019-03-25 12:43:17 +0530

    Solved Java Introduction!

---
+++ src/Java/Introduction/Welcome_to_Java.java
@@ -0,0 +1,7 @@
+package Java.Introduction;
+
+public class Welcome_to_Java {
+    public static void main(String[] args) {
+        System.out.printf("%s%n%s","Hello, World","Hello, Java");
+    }
+}
