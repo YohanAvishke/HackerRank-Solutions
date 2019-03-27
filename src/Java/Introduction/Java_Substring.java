6a9d7bb53db9272d614cd6916f73a7e2f46bc002
YohanAvishke <yohan.2017213@iit.ac.lk>
2019-03-27 08:38:56 +0530

    Solved Java Substring

---
+++ src/Java/Introduction/Java_Substring.java
@@ -0,0 +1,17 @@
+package Java.Introduction;
+
+import java.util.*;
+
+public class Java_Substring {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+
+        System.out.println(S.substring(start,end));
+    }
+}
+
+
